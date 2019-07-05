8e2c703082f9b5cd174e6e6d99eebbceeab3eeed
leontev <andruwwwka@gmail.com>
2019-07-05 23:53:39 +0300

    Добавил шаблон для описания методов во фласгере

--- rebus_hackaton/settings.py
+++ rebus_hackaton/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
+    'app_templates',
 ]
 
 MIDDLEWARE = [
--- rebus_hackaton/urls.py
+++ rebus_hackaton/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Rebus application API')
 
 urlpatterns = [
-    path('apidocs/', schema_view),
     path('admin/', admin.site.urls),
+    path('app_templates/', include('app_templates.urls'), name='app_templates'),
+    path('apidocs/', schema_view),
 ]
