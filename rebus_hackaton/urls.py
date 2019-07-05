b5413ba66eab29ba3802c3cd13015e5016a7c844
leontev <andruwwwka@gmail.com>
2019-07-05 22:25:23 +0300

    Добавил swagger

--- rebus_hackaton/urls.py
+++ rebus_hackaton/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Rebus application API')
 
 urlpatterns = [
+    path(r'^$', schema_view),
     path('admin/', admin.site.urls),
 ]
