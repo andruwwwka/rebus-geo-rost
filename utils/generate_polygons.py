23087621d739d31a2472db9c283b85f9b4180e75
leontev <andruwwwka@gmail.com>
2019-07-07 02:20:32 +0300

    Игнорирование полигонов, которые не попадают в Ярославль

--- utils/generate_polygons.py
+++ utils/generate_polygons.py
@@ -1,6 +1,7 @@
 from itertools import takewhile, count
 
 from geo_data.models import Polygon
+from utils.yandex_decoder_wrapper import YandexDecodeApi
 
 
 class PolygonDataGenerator:
@@ -11,11 +12,14 @@ class PolygonDataGenerator:
 
     @classmethod
     def generate_polygons(cls):
+        api = YandexDecodeApi()
         for latitude in cls.range_for_float(57.520544, 57.777502, .005001):
             for longitude in cls.range_for_float(39.695266, 40.027602, .005001):
-                Polygon.objects.create(
-                    lat1=round(latitude, 6),
-                    lon1=round(longitude, 6),
-                    lat2=round(latitude + .005, 6),
-                    lon2=round(longitude + .005, 6)
-                )
+                coordinate = (latitude + .002500, longitude + .002500)
+                if api.get_city(coordinate) == 'Ярославль':
+                    Polygon.objects.create(
+                        lat1=round(latitude, 6),
+                        lon1=round(longitude, 6),
+                        lat2=round(latitude + .005, 6),
+                        lon2=round(longitude + .005, 6)
+                    )
--- utils/yandex_decoder_wrapper.py
+++ utils/yandex_decoder_wrapper.py
@@ -12,10 +12,13 @@ class YandexDecodeApi():
         self.api_key = settings.API_KEY
         self.format = format
 
-    def get_coordinate(self, address):
-        response_data = requests.get(
-            f'https://geocode-maps.yandex.ru/1.x/?apikey={self.api_key}&format={self.format}&geocode={address}'
+    def get_response(self, geocode):
+        return requests.get(
+            f'https://geocode-maps.yandex.ru/1.x/?apikey={self.api_key}&format={self.format}&geocode={geocode}'
         ).json()
+
+    def get_coordinate(self, address):
+        response_data = self.get_response(address)
         try:
             longitude, latitude = response_data.get('response').\
                 get('GeoObjectCollection').get('featureMember')[0].\
@@ -23,3 +26,20 @@ class YandexDecodeApi():
         except (KeyError, IndexError):
             raise YandexDecodeApiException
         return latitude, longitude
+
+    def get_city(self, coordinate):
+        response_data = self.get_response(f'{coordinate[1]},{coordinate[0]}')
+        print(response_data)
+        try:
+            city = ''
+            address_components = response_data.get('response'). \
+                get('GeoObjectCollection').get('featureMember')[0]. \
+                get('GeoObject').get('metaDataProperty').get('GeocoderMetaData'). \
+                get('Address').get('Components')
+            for component in address_components:
+                if component['kind'] == 'locality':
+                    city = component['name']
+                    break
+        except (KeyError, IndexError):
+            raise YandexDecodeApiException
+        return city
