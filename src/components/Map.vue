022c4996bdf2fe75a9e672104e9d373495a0467e
Ilya Pro <ilya.pro.dev@gmail.com>
2019-07-06 21:27:57 +0300

    add ui

---
+++ src/components/Map.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="reb-Map">
+        <span style="position: absolute; background-color: red; color: white;">Map</span>
+        <!--
+        :coords="[54.62896654088406, 39.731893822753904]"
+        :behaviors="['ruler']"
+        :controls="['trafficControl']"
+        -->
+        <yandex-map
+                :settings="settings"
+                :coords="[57.617499, 39.924038]"
+                zoom="10"
+                style="width: 100%; height: 100%;"
+                :cluster-options="{
+                1: {clusterDisableClickZoom: true}
+                }"
+                :behaviors="[]"
+                :controls="[]"
+                :placemarks="placemarks"
+                map-type="map"
+                @map-was-initialized="initHandler"
+        />
+    </div>
+
+</template>
+
+<!--<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=c55b9816-9c45-4a43-a45a-4505c813e193" type="text/javascript"></script>-->
+<script>
+    import { yandexMap/*, ymapMarker*/ } from 'vue-yandex-maps'
+
+    /*const settings = {
+        apiKey: 'c55b9816-9c45-4a43-a45a-4505c813e193',
+        lang: 'ru_RU',
+        coordorder: 'latlong',
+        version: '2.1'
+    };
+*/
+    //Vue.use(yandexMap, settings);
+
+    export default {
+        name: "Map",
+        props: {
+            msg: String
+        },
+        data () {
+            return {
+                settings: {
+                    apiKey: 'c55b9816-9c45-4a43-a45a-4505c813e193',
+                    lang: 'ru_RU',
+                    coordorder: 'latlong',
+                    version: '2.1'
+                },
+                placemarks: []
+            }
+        },
+        methods: {
+            initHandler () {
+                console.log('initHandler ГОТОВО');
+            }
+        },
+        components: {
+            yandexMap/*, ymapMarker*/
+        }
+    }
+</script>
+
+<style>
+.reb-Map {
+    width: 100%;
+    height: 100%;
+}
+</style>
---
+++ src/components/MapContainer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="reb-MapContainer">
+        <span style="position: absolute; background-color: red; color: white;">MapCont</span>
+        <Map/>
+        <CatFilter/>
+    </div>
+</template>
+
+<script>
+    import Map from '@/components/Map.vue'
+    import CatFilter from "./CatFilter";
+
+    export default {
+        name: "MapContainer",
+        components: {
+            CatFilter,
+            Map
+        }
+    }
+</script>
+
+<style>
+.reb-MapContainer {
+    height: 100%;
+}
+</style>
