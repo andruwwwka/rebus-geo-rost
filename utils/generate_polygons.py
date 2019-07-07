cedaa0c3437bbb394b0343d9ed966e6f9ff2e527
leontev <andruwwwka@gmail.com>
2019-07-07 06:55:10 +0300

    Изменил размер полигона

--- utils/generate_polygons.py
+++ utils/generate_polygons.py
@@ -13,13 +13,13 @@ class PolygonDataGenerator:
     @classmethod
     def generate_polygons(cls):
         api = YandexDecodeApi()
-        for latitude in cls.range_for_float(57.520544, 57.777502, .005001):
-            for longitude in cls.range_for_float(39.695266, 40.027602, .005001):
-                coordinate = (latitude + .002500, longitude + .002500)
+        for latitude in cls.range_for_float(57.520544, 57.777502, .010001):
+            for longitude in cls.range_for_float(39.695266, 40.027602, .010001):
+                coordinate = (latitude + .005000, longitude + .005000)
                 if api.get_city(coordinate) == 'Ярославль':
                     Polygon.objects.create(
                         lat1=round(latitude, 6),
                         lon1=round(longitude, 6),
-                        lat2=round(latitude + .005, 6),
-                        lon2=round(longitude + .005, 6)
+                        lat2=round(latitude + .01, 6),
+                        lon2=round(longitude + .01, 6)
                     )
