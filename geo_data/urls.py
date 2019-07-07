dbdd482a6d9869ab924d0086e106b772c6730532
leontev <andruwwwka@gmail.com>
2019-07-07 06:32:55 +0300

    Добавил фикстуры для загрузки точек на карте

--- geo_data/urls.py
+++ geo_data/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
 
-from geo_data.resources import TestPolygonViewSet
+from geo_data.resources import TestPolygonViewSet, PolygonViewSet
 
 router = routers.DefaultRouter()
 router.register(r'test', TestPolygonViewSet, 'test')
+router.register(r'ratings', PolygonViewSet, 'ratings')
 
 urlpatterns = router.urls
--- rebus_hackaton/urls.py
+++ rebus_hackaton/urls.py
@@ -23,7 +23,6 @@ schema_view = get_swagger_view(title='Rebus application API')
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
-    path('app_templates/', include('app_templates.urls'), name='app_templates'),
     path('geo_data/', include('geo_data.urls'), name='geo_data'),
     path('apidocs/', schema_view),
 ]
