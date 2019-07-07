c08d16e2a002f7d62777c39da8b535253ac62c32
Ilya Pro <ilya.pro.dev@gmail.com>
2019-07-07 12:49:15 +0300

    ui change filter for data

--- src/components/CatFilter.vue
+++ src/components/CatFilter.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="reb-CatFilter">
         <div class="reb-CatFilter__list">
-            <div>Школы</div>
-            <div class="reb-CatFilter__item-inactive">Детские сады</div>
-            <div>Медицина</div>
-            <div>Спортивные объекты</div>
-            <div>Общественный транспорт</div>
+            <div v-for="item in items"
+                 class="reb-CatFilter__item"
+                 :class="{ 'reb-CatFilter__item-inactive': !item.enabled }"
+                 v-on:click="itemClick(item)"
+            >{{ item.name }}</div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "CatFilter"
+        name: "CatFilter",
+        props: {
+            items: Array
+        },
+        data () {
+            return {
+
+            }
+        },
+        methods: {
+            itemClick (item) {
+                item.enabled = !item.enabled;
+                this.$emit('filter-change', this.items);
+            }
+        }
     }
 </script>
 
@@ -22,7 +36,7 @@
         top: 20px;
         left: 20px;
         border: blue;
-        padding: 20px;
+        padding: 10px 0;
         background-color: white;
         border-radius: 8px;
         box-shadow: 5px 2px 9px rgba(0,0,0,0.2);
@@ -32,8 +46,13 @@
         line-height: 2;
     }
     .reb-CatFilter__item {
-
+        cursor: pointer;
+        padding: 0 20px;
     }
+    .reb-CatFilter__item:hover {
+        background-color: #EBF2F7;
+    }
+
     .reb-CatFilter__item-inactive {
         color: #949494;
     }
