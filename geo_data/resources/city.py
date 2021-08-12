ea19dbdd20a001e95957894645c77199479cebe7
Andrey Leontyev <andruwwwka@gmail.com>
2021-08-13 01:40:27 +0300

    Добавил потерянный queryset

--- geo_data/resources/city.py
+++ geo_data/resources/city.py
@@ -1,10 +1,12 @@
 """Представления для управления городами."""
 from rest_framework.viewsets import ModelViewSet
 
+from ..models import City
 from ..serializers import CitySerializer
 
 
 class CityViewSet(ModelViewSet):
     """Представление городов."""
 
+    quryset = City.objects.all()
     serializer_class = CitySerializer
